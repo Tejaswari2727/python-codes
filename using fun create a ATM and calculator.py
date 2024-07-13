1e781e30d259931e8198cffad261ce4d04001495
Tejaswari2727 <125174316+Tejaswari2727@users.noreply.github.com>
2024-07-13 14:45:21 +0530

    Add files via upload

---
+++ using fun create a ATM and calculator.py
@@ -0,0 +1,58 @@
+def addition (a,b):
+    print("a+b=",a+b)
+def substraction (a,b):
+    print("a-b=",a-b)
+def multiplication (a,b):
+    print("a*b=",a*b)
+def division (a,b):
+    print("a/b=",a/b)
+print("1.Addition")
+print("2.Substraction")
+print("3.Multiplication")
+print("4.Division")
+option=int(input("select the choice from option1/2/3/4:"))
+a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+if option==1:
+    print("Addition is:",a+b)
+elif option==2:
+    print("Substraction is:",a-b)
+elif option==3:
+    print("Multiplication is:",a*b)
+elif option==4:
+    print("Division is:",a/b)
+else:
+    print("invalid operator")
+
+
+
+
+= RESTART: C:/Users/batti/OneDrive/Desktop/FULL STAC PYTHON/python classes/using fun create a ATM and calculator.py
+1.Addition
+2.Substraction
+3.Multiplication
+4.Division
+select the choice from option1/2/3/4:1
+enter a value:3
+enter b value:4
+Addition is: 7
+
+= RESTART: C:/Users/batti/OneDrive/Desktop/FULL STAC PYTHON/python classes/using fun create a ATM and calculator.py
+1.Addition
+2.Substraction
+3.Multiplication
+4.Division
+select the choice from option1/2/3/4:2
+enter a value:3
+enter b value:6
+Substraction is: -3
+
+= RESTART: C:/Users/batti/OneDrive/Desktop/FULL STAC PYTHON/python classes/using fun create a ATM and calculator.py
+1.Addition
+2.Substraction
+3.Multiplication
+4.Division
+select the choice from option1/2/3/4:3
+enter a value:5
+enter b value:3
+Multiplication is: 15
